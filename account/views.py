a07c7306d0308c813ea5359ede411a9e3525fda1
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-05 17:30:36 +0900

    ✨ (account): Moves wrong codes

--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from accounts.views import LoginView
 
-# Create your views here.
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        email = request.data.get("email")
+        password = request.data.get("password")
+        user = authenticate(request, email=email, password=password)  # 사용자 인증
+        if user is not None:
+            login(request, user)  # 로그인 처리 (세션 생성)
+            return Response({"message": "로그인 성공"})
+        else:
+            return Response({"message": "로그인 실패"}, status=401)
--- config/settings.py
+++ config/settings.py
@@ -103,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "account.CustomUser"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
