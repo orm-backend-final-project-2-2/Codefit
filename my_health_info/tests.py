9a157cb7d2f4e49acb9a7915361d05bb9550edb1
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-06 02:05:36 +0900

    ♻️ (my_health_info): Refactor tests.py to use Faker library for generating fake user and health info data

--- my_health_info/tests.py
+++ my_health_info/tests.py
@@ -3,35 +3,45 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from my_health_info.models import HealthInfo
 from freezegun import freeze_time
+from faker import Faker
 
 
 class MyHealthInfoTestCase(TestCase):
     def setUp(self):
-        self.user1 = User.objects.create_user(
-            username="testuser", password="testpassword"
+        self.fake = Faker()
+
+        self.user1 = self.create_fake_user()
+        self.user1_health_info = self.create_fake_health_info(self.user1)
+        self.user1_bmi = self.calculate_users_bmi(self.user1_health_info)
+
+        self.user2 = self.create_fake_user()
+        self.user2_health_info = self.create_fake_health_info(self.user2)
+
+    def create_fake_user(self):
+        fake_user_info = {
+            "username": self.fake.user_name(),
+            "password": self.fake.password(),
+        }
+
+        User.objects.create_user(**fake_user_info)
+        return fake_user_info
+
+    def create_fake_health_info(self, user):
+        fake_health_info = {
+            "user": User.objects.get(username=user["username"]),
+            "age": self.fake.random_int(min=1, max=70),
+            "height": self.fake.random_int(min=100, max=200),
+            "weight": self.fake.random_int(min=30, max=150),
+        }
+
+        HealthInfo.objects.create(
+            **fake_health_info,
         )
 
-        self.user1_health_info = HealthInfo.objects.create(
-            user=self.user1,
-            age=20,
-            height=170,
-            weight=60,
-        )
+        return fake_health_info
 
-        self.user1_bmi = self.user1_health_info.weight / (
-            (self.user1_health_info.height / 100) ** 2
-        )
-
-        self.user2 = User.objects.create_user(
-            username="testuser2", password="testpassword2"
-        )
-
-        self.user2_health_info = HealthInfo.objects.create(
-            user=self.user2,
-            age=25,
-            height=175,
-            weight=70,
-        )
+    def calculate_users_bmi(self, health_info):
+        return health_info.get("weight") / ((health_info.get("height") / 100) ** 2)
 
     def test_get_my_health_info_not_authenticated(self):
         """비로그인 유저가 접근할 때 403 에러를 리턴하는지 테스트"""
@@ -41,56 +51,69 @@ class MyHealthInfoTestCase(TestCase):
 
     def test_get_my_health_info_user1(self):
         """유저1이 접근할 때 유저1의 건강 정보를 리턴하는지 테스트"""
-        self.client.login(username="testuser", password="testpassword")
+        self.client.login(
+            username=self.user1.get("username"), password=self.user1.get("password")
+        )
         response = self.client.get(reverse("my_health_info"))
 
         self.assertEqual(response.status_code, 200)
 
-        self.assertEqual(response.json().get("age"), self.user1_health_info.age)
-        self.assertEqual(response.json().get("height"), self.user1_health_info.height)
-        self.assertEqual(response.json().get("weight"), self.user1_health_info.weight)
+        self.assertEqual(response.json().get("age"), self.user1_health_info.get("age"))
+        self.assertEqual(
+            response.json().get("height"), self.user1_health_info.get("height")
+        )
+        self.assertEqual(
+            response.json().get("weight"), self.user1_health_info.get("weight")
+        )
         self.assertEqual(response.json().get("bmi"), self.user1_bmi)
 
     def test_get_my_health_info_user2(self):
         """유저2가 접근할 때 유저2의 건강 정보를 리턴하는지 테스트"""
-        self.client.login(username="testuser2", password="testpassword2")
+        self.client.login(
+            username=self.user2.get("username"), password=self.user2.get("password")
+        )
         response = self.client.get(reverse("my_health_info"))
 
         self.assertEqual(response.status_code, 200)
 
-        user2_bmi = self.user2_health_info.weight / (
-            (self.user2_health_info.height / 100) ** 2
-        )
+        user2_bmi = self.calculate_users_bmi(self.user2_health_info)
 
-        self.assertEqual(response.json().get("age"), self.user2_health_info.age)
-        self.assertEqual(response.json().get("height"), self.user2_health_info.height)
-        self.assertEqual(response.json().get("weight"), self.user2_health_info.weight)
+        self.assertEqual(response.json().get("age"), self.user2_health_info.get("age"))
+        self.assertEqual(
+            response.json().get("height"), self.user2_health_info.get("height")
+        )
+        self.assertEqual(
+            response.json().get("weight"), self.user2_health_info.get("weight")
+        )
         self.assertEqual(response.json().get("bmi"), user2_bmi)
 
     @freeze_time("2023-01-01")
     def test_get_my_health_info_last(self):
         """접근 시 마지막으로 생성된 건강 정보를 리턴하는지 테스트"""
-        HealthInfo.objects.create(
-            user=self.user1,
-            age=19,
-            height=175,
-            weight=50,
-        )
+        new_health_info = self.create_fake_health_info(self.user1)
 
-        self.client.login(username="testuser", password="testpassword")
+        self.client.login(
+            username=self.user1.get("username"), password=self.user1.get("password")
+        )
 
         response = self.client.get(reverse("my_health_info_last"))
 
         self.assertEqual(response.status_code, 200)
 
-        self.assertEqual(response.json().get("age"), self.user1_health_info.age)
-        self.assertEqual(response.json().get("height"), self.user1_health_info.height)
-        self.assertEqual(response.json().get("weight"), self.user1_health_info.weight)
+        self.assertEqual(response.json().get("age"), self.user1_health_info.get("age"))
+        self.assertEqual(
+            response.json().get("height"), self.user1_health_info.get("height")
+        )
+        self.assertEqual(
+            response.json().get("weight"), self.user1_health_info.get("weight")
+        )
         self.assertEqual(response.json().get("bmi"), self.user1_bmi)
 
     def test_post_my_health_info(self):
         """POST 요청으로 건강 정보를 생성하는지 테스트"""
-        self.client.login(username="testuser", password="testpassword")
+        self.client.login(
+            username=self.user1.get("username"), password=self.user1.get("password")
+        )
 
         new_health_info = {
             "age": 21,
@@ -117,7 +140,9 @@ class MyHealthInfoTestCase(TestCase):
 
     def test_post_my_health_info_with_invalid_age(self):
         """POST 요청으로 나이가 음수인 건강 정보를 생성할 때 400 에러를 리턴하는지 테스트"""
-        self.client.login(username="testuser", password="testpassword")
+        self.client.login(
+            username=self.user1.get("username"), password=self.user1.get("password")
+        )
 
         new_health_info = {
             "age": -1,
