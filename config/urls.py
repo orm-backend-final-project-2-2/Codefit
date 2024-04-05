dcd9a1480cebebad20ba4e25d58ca0e008b7c334
soohyun020812 <amd090108lsh02@gmail.com>
2024-04-05 20:00:23 +0900

    ✨ (config): commit

--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path("account/", include("account.urls")),
-    # path("community/", include("community.urls")),
+    path("community/", include("community.urls")),
     # path("my_health_info/", include("my_health_info.urls")),
     # path("exercises_info/", include("exercises_info.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
