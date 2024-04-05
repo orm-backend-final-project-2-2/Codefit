47484225ffe0b2e9851b703b17e315d11fe14391
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-06 01:13:44 +0900

    🎨 (my_health_info): Add read-only fields to HealthInfoSerializer
    serializers.PrimaryKeyRelatedField를 real_only_fields로 교체했습니다.

--- my_health_info/serializers.py
+++ my_health_info/serializers.py
@@ -3,8 +3,7 @@ from my_health_info.models import HealthInfo
 
 
 class HealthInfoSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(read_only=True)
-
     class Meta:
         model = HealthInfo
         fields = ["user", "age", "height", "weight", "bmi", "created_at"]
+        read_only_fields = ["user"]
