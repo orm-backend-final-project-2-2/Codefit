94ad3dfebc49689e712ab831542528734e209e1f
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-05 16:52:16 +0900

    ✨ (my_health_info): Create MyHealthInfo model
    MyHealInfo 모델 구현했습니다.

--- my_health_info/models.py
+++ my_health_info/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class HealthInfo(models.Model):
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    age = models.IntegerField()
+    height = models.FloatField()
+    weight = models.FloatField()
+    bmi = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}의 건강 정보"
+
+    def save(self, *args, **kwargs):
+        self.bmi = self.weight / ((self.height / 100) ** 2)
+        super().save(*args, **kwargs)
