4a13d748de0f40dc84af856ad705f0d6e284e4bf
limvin <vinpage00@gmail.com>
2024-04-06 21:19:03 +0900

    ✨ (exercises_info): Create ExercisesInfo model

--- exercises_info/models.py
+++ exercises_info/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ExercisesInfo(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="exercises_info"
+    )
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    video = models.ImageField(upload_to="exercises_info_video/", blank=True)
+
+    def __str__(self):
+        return f"{self.title}"
