358241a6618afd74ae6282cf77a0894a9cfa73b5
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-06 10:29:35 +0900

    👷(utils): Update script for CI

--- utils/parse_feature_branch.py
+++ utils/parse_feature_branch.py
@@ -3,11 +3,12 @@ import re
 
 if len(sys.argv) == 2:
     branch_name = sys.argv[1]
-    pattern = re.compile(r".*feature-(\w+)/(.+)")
+    pattern = re.compile(r".*feature-(.+)/(.+)")
     matches = pattern.match(branch_name)
 
     if matches:
         app_name, feature_name = matches.groups()
+        app_name = app_name.replace("-", "_")
         words = feature_name.split("-")
         tc_feature_name = "".join([word.title() for word in words]) + "TestCase"
         print(f"{app_name} {tc_feature_name}")
