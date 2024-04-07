71391d780ac1af8fff11a7a8e4dbd1f8cca97f6a
AnHyoJun <neysin@naver.com>
2024-04-08 01:59:21 +0900

    🔧  (account): add urlspatterns

---
+++ account/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from account import views
+
+router = DefaultRouter()
+router.register(r"account", views.CustomUserViewSet, basename="account")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
