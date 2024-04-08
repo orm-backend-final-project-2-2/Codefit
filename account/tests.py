e5dd902c5b4076914c61de15c5fbd98fb654a01b
AnHyoJun <neysin@naver.com>
2024-04-08 11:33:46 +0900

    ✅ (account): Add test test_login_by_wrong_password

--- account/tests.py
+++ account/tests.py
@@ -36,4 +36,11 @@ class LoginTestCase(TestCase):
         self.assertEqual(response.data["message"], "로그인 실패")
 
     def test_login_by_wrong_password(self):
-        
+        """
+        테스트 로그인이 비밀번호가 다를 때 실패하는지 확인
+        """
+        new_password = self.faker.password()
+        wrong_user_info = {"email": self.user1_info["email"], "password": new_password}
+        response = self.client.post(reverse("login"), data=wrong_user_info)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data["message"], "로그인 실패")
