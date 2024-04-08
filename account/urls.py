87ec2de03bd8af6042277323fd05717b27c16c8f
AnHyoJun <neysin@naver.com>
2024-04-08 12:31:07 +0900

    🎨 (account): Add logout, signup, profile url

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from account import views
-
-router = DefaultRouter()
-router.register(r"account", views.CustomUserViewSet, basename="account")
+from django.urls import path
+from account.views import LoginView, LogoutView, SignUpView, ProfileView
 
 urlpatterns = [
-    path("", include(router.urls)),
-    path("login/", views.LoginView.as_view(), name="login"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
