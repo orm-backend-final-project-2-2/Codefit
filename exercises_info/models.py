843f07b0fdb672ae0a86c504b6e37846ddc56251
limvin <vinpage00@gmail.com>
2024-04-08 16:19:44 +0900

    ✨ (exercises_info): FocusArea 모델 주석 처리

--- exercises_info/models.py
+++ exercises_info/models.py
@@ -9,25 +9,25 @@ class ExercisesInfo(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     video = models.ImageField(upload_to="exercises_info_video/", blank=True)
-    focus_areas = models.ManyToManyField("FocusArea", related_name="exercise")
-    exercise_attributes = models.OneToOneField(
-        "ExercisesAttribute", on_delete=models.CASCADE, related_name="exercise"
-    )
+    # focus_areas = models.ManyToManyField("FocusArea", related_name="exercise")
+    # exercise_attributes = models.OneToOneField(
+    #     "ExercisesAttribute", on_delete=models.CASCADE, related_name="exercise"
+    # )
 
     def __str__(self):
         return f"{self.title}"
 
 
-class FocusArea(models.Model):
-    focus_area = models.CharField(max_length=100)
+# class FocusArea(models.Model):
+#     focus_area = models.CharField(max_length=100)
 
-    def __str__(self):
-        return f"{self.focus_area}"
+#     def __str__(self):
+#         return f"{self.focus_area}"
 
 
-class ExercisesAttribute(models.Model):
-    need_set = models.BooleanField(default=False)
-    need_rep = models.BooleanField(default=False)
-    need_weight = models.BooleanField(default=False)
-    need_duration = models.BooleanField(default=False)
-    need_speed = models.BooleanField(default=False)
+# class ExercisesAttribute(models.Model):
+#     need_set = models.BooleanField(default=False)
+#     need_rep = models.BooleanField(default=False)
+#     need_weight = models.BooleanField(default=False)
+#     need_duration = models.BooleanField(default=False)
+#     need_speed = models.BooleanField(default=False)
