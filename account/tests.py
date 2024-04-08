fe3fb363f3d3d5c276c8830cab682ff8a167fc88
AnHyoJun <neysin@naver.com>
2024-04-08 11:05:31 +0900

    ✅ (account): Add login test

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from rest_framework import status
+from django.urls import reverse
+from account.models import CustomUser
 
-# Create your tests here.
+
+class LoginTestCase(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            email="test@example.com", password="testpassword", nickname="testuser"
+        )
+
+    def test_user_can_login(self):
+        """
+        테스트 로그인이 성공하는지 확인
+        """
+        data = {"email": "test@example.com", "password": "testpassword"}
+        response = self.client.post(
+            reverse("login"),
+            data=data,
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["message"], "로그인 성공")
