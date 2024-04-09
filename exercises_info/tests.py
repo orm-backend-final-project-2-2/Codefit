6751bd67d0312ba2eb4f2c2f4c7b07745f8f0445
limvin <vinpage00@gmail.com>
2024-04-09 11:09:33 +0900

    ✅  (exercises_info): 운동 정보 수정 테스트 케이스 추가

--- exercises_info/tests.py
+++ exercises_info/tests.py
@@ -1,3 +1,4 @@
+import json
 from django.test import Client, TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
@@ -77,3 +78,35 @@ class ExercisesInfoTestCase(TestCase):
         )
         self.assertEqual(response.status_code, 403)
         self.assertEqual(ExercisesInfo.objects.count(), 2)
+
+    # 관리자만 운동 정보를 수정할 수 있는지 확인
+    def test_admin_can_update_exercise(self):
+        self.client.login(**self.admin_info)
+
+        # 데이터 전송 시 Content-Type 설정
+        content_type = "application/json"
+        data = json.dumps(self.exercise2_request)
+
+        response = self.client.put(
+            reverse("exercisesinfo-detail", args=[self.exercise1.id]),
+            data=data,
+            content_type=content_type,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            ExercisesInfo.objects.get(id=self.exercise1.id).title,
+            self.exercise2.title,
+        )
+
+    # 일반유저가 운동 정보를 수정할 수 있는지 확인
+    def test_user_can_update_exercise(self):
+        self.client.login(**self.user_info)
+        response = self.client.put(
+            reverse("exercisesinfo-detail", args=[self.exercise1.id]),
+            data=self.exercise2_request,
+        )
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(
+            ExercisesInfo.objects.get(id=self.exercise1.id).title,
+            self.exercise1.title,
+        )
