43ba34cc390cd7932c739c480889a8a853547caa
soohyun020812 <amd090108lsh02@gmail.com>
2024-04-10 13:58:45 +0900

    ✅ (community): 테스트 코드 추가 및 제거

--- community/tests.py
+++ community/tests.py
@@ -106,8 +106,6 @@ class PostTestCase(TestCase):
 
     """수정 중"""
 
-
-'''
     def test_delete_post_without_login(self):
         """post/<pk>/ DELETE 요청시 로그인하지 않은 경우 403 에러를 반환하는지 테스트"""
         response = self.client.delete(
@@ -136,10 +134,13 @@ class PostTestCase(TestCase):
         response = self.client.get(reverse("post-list"))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            len(response.data), 2
-        )  # 숫자 대신 원하는 값을 직접 쿼리로 가져와서 비교하는게 좋습니다.
+        # self.assertEqual(
+        #     len(response.data), 2
+        # )  # 숫자 대신 원하는 값을 직접 쿼리로 가져와서 비교하는게 좋습니다.
+        self.assertEqual(response.data[0]["title"], self.user1_post1.instance.title)
 
+
+'''
     # TODO: 다른사람도 포스트를 볼 수 있어야 합니다
     def test_get_only_logged_in_user_post_detail(self):
         """post/<pk>/ GET 요청시 로그인한 사용자의 특정 Post 객체만 반환하는지 테스트"""
