376d3c0f02df42c72a53e756d7b62eeefd6b0ab8
AnHyoJun <neysin@naver.com>
2024-04-11 12:28:30 +0900

    🔧 (account): 비밀번호 유효성 검사에 숫자, 문자, 특수문자 포함 여부 확인 추가

--- account/serializers.py
+++ account/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 from account.models import CustomUser
+import re
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=20, min_length=8, write_only=True)
+
+    def validate_password(self, value):
+        # 비밀번호에 숫자, 문자, 특수문자를 모두 포함하는지 확인하는 정규 표현식
+        if not re.match(r"^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*()-_+=]).*$", value):
+            raise serializers.ValidationError(
+                "비밀번호는 숫자, 문자, 특수문자를 모두 포함해야 합니다."
+            )
+        return value
 
     class Meta:
         model = CustomUser
