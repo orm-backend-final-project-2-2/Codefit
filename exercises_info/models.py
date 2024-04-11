005c333362f8cabdd060347afb9e73beb02666e5
limvin <vinpage00@gmail.com>
2024-04-11 17:15:55 +0900

    ✨ (exercises_info): 운동 정보 모델의 focus_areas 필드를 ManyToManyField로 변경

--- exercises_info/models.py
+++ exercises_info/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from account.models import CustomUser as User
+from utils.enums import FocusAreaEnum
 
 
 class ExercisesInfo(models.Model):
@@ -9,30 +10,15 @@ class ExercisesInfo(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     video = models.FileField(upload_to="exercises_info_video/", blank=True)
-    focus_areas = models.ManyToManyField("FocusArea", related_name="exercise")
+    focus_areas = models.ManyToManyField(
+        FocusAreaEnum, verbose_name="Focus Areas", related_name="exercise"
+    )
     # exercise_attributes = models.OneToOneField(
     #     "ExercisesAttribute", on_delete=models.CASCADE, related_name="exercise"
     # )
 
     def __str__(self):
         return f"{self.title}"
-
-
-class FocusArea(models.Model):
-    FOCUS_AREA_CHOICES = [
-        ("유산소", "유산소"),
-        ("가슴", "가슴"),
-        ("등", "등"),
-        ("어깨", "어깨"),
-        ("팔", "팔"),
-        ("하체", "하체"),
-        ("코어", "코어"),
-    ]
-
-    focus_area = models.CharField(max_length=20, choices=FOCUS_AREA_CHOICES)
-
-    def __str__(self):
-        return self.get_focus_area_display()
 
 
 # class ExercisesAttribute(models.Model):
