ef79f013692fe2037952f0ff835ac88ad50f0765
AnHyoJun <neysin@naver.com>
2024-04-11 11:58:07 +0900

    ✅ (account): Add test_sign_up_by_blank_username
    테스트 닉네임이 공백일 때 실패하는지 확인하는 테스트 케이스를 추가하였습니다.

--- account/tests.py
+++ account/tests.py
@@ -138,3 +138,13 @@ class SignUpTestCase(TestCase):
             response.data["username"][0],
             "이 필드의 글자 수가 20 이하인지 확인하십시오.",
         )
+
+    def test_sign_up_by_blank_username(self):
+        """
+        테스트 닉네임이 공백일 때 실패하는지 확인
+        """
+        self.user1.fake_info["username"] = ""
+
+        response = self.client.post(reverse("signup"), data=self.user1.request_create())
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["username"][0], "이 필드는 blank일 수 없습니다.")
