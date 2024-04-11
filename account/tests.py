b1f0b2e5f91645b4a6e25cf1b1dadf28bf56abe8
AnHyoJun <neysin@naver.com>
2024-04-11 10:31:43 +0900

    ✅ (account): Add test_sign_up_by_existing_email

--- account/tests.py
+++ account/tests.py
@@ -64,29 +64,26 @@ class SignUpTestCase(TestCase):
         self.assertEqual(response.data["username"], self.user1.fake_info["username"])
         self.assertEqual(response.data["email"], self.user1.fake_info["email"])
 
-    # def test_sign_up_by_existing_username(self):
-    #     """
-    #     테스트 이미 존재하는 닉네임일 때 실패하는지 확인
-    #     """
-    #     self.user1.create_instance()
+    def test_sign_up_by_existing_username(self):
+        """
+        테스트 이미 존재하는 닉네임일 때 실패하는지 확인
+        """
+        self.user1.create_instance()
 
-    #     response = self.client.post(reverse("signup"), data=self.user1.request_create())
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-    #     self.assertEqual(
-    #         response.data["username"][0], "사용자의 닉네임은/는 이미 존재합니다."
-    #     )
+        response = self.client.post(reverse("signup"), data=self.user1.request_create())
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data["username"][0], "사용자의 닉네임은/는 이미 존재합니다."
+        )
 
-    # def test_sign_up_by_existing_email(self):
-    #     """
-    #     테스트 이미 존재하는 이메일일 때 실패하는지 확인
-    #     """
-    #     self.user1.create_instance()
+    def test_sign_up_by_existing_email(self):
+        """
+        테스트 이미 존재하는 이메일일 때 실패하는지 확인
+        """
+        self.user1.create_instance()
 
-    #     new_user = FakeUser()
-    #     new_user.fake_info["email"] = self.user1.fake_info["email"]
-
-    #     response = self.client.post(reverse("signup"), data=new_user.request_create())
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-    #     self.assertEqual(
-    #         response.data["email"][0], "사용자의 이메일 주소는/은 이미 존재합니다."
-    #     )
+        response = self.client.post(reverse("signup"), data=self.user1.request_create())
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data["email"][0], "사용자의 이메일은/는 이미 존재합니다."
+        )
