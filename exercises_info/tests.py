fa048b95bec56d49f7f000b1ea5254ee9bb43795
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-13 17:09:30 +0900

    ✨ (exercises_info): Refactor tests.py to update test cases and fix bugs

--- exercises_info/tests.py
+++ exercises_info/tests.py
@@ -13,27 +13,19 @@ class ExercisesInfoTestCase(TestCase):
     def setUp(self):
         self.admin = FakeUser()
         self.admin.create_instance(is_staff=True)
-        self.admin_info = {
-            "username": self.admin.instance.username,
-            "password": "password",
-        }
 
         self.user = FakeUser()
         self.user.create_instance()
-        self.user_info = {
-            "username": self.user.instance.username,
-            "password": "password",
-        }
 
         self.exercise1 = FakeExercisesInfo()
-        self.exercise1.create_instance(user_instance=self.user.instance)
+        self.exercise1.create_instance(user_instance=self.admin.instance)
 
         self.exercise2 = FakeExercisesInfo()
-        self.exercise2.create_instance(user_instance=self.user.instance)
+        self.exercise2.create_instance(user_instance=self.admin.instance)
 
     # 모든 사용자가 운동 정보 리스트를 볼 수 있는지 확인
     def test_all_users_can_view_exercises_list(self):
-        response = self.client.get(reverse("exercisesinfo-list"))
+        response = self.client.get(reverse("exercises-info-list"))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertContains(response, self.exercise1.instance.title)
@@ -42,7 +34,7 @@ class ExercisesInfoTestCase(TestCase):
     # 모든 사용자가 운동 정보 상세를 볼 수 있는지 확인
     def test_all_users_can_view_exercise_detail(self):
         response = self.client.get(
-            reverse("exercisesinfo-detail", kwargs={"pk": self.exercise1.instance.id})
+            reverse("exercises-info-detail", kwargs={"pk": self.exercise1.instance.id})
         )
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.exercise1.instance.title)
@@ -53,9 +45,12 @@ class ExercisesInfoTestCase(TestCase):
 
         new_exercise = FakeExercisesInfo()
 
+        print(new_exercise.request_create())
+
         response = self.client.post(
-            reverse("exercisesinfo-list"),
+            reverse("exercises-info-list"),
             data=new_exercise.request_create(),
+            content_type="application/json",
         )
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -71,66 +66,66 @@ class ExercisesInfoTestCase(TestCase):
         self.client.force_login(self.user.instance)
 
         response = self.client.post(
-            reverse("exercisesinfo-list"),
+            reverse("exercises-info-list"),
             data=new_exercise.request_create(),
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     # 관리자만 운동 정보를 수정할 수 있는지 확인
     def test_admin_can_update_exercise(self):
-        self.client.login(**self.admin_info)
+        self.client.force_login(self.admin.instance)
 
-        # 데이터 전송 시 Content-Type 설정
-        content_type = "application/json"
-        data = json.dumps(self.exercise2_request)
+        new_exercise = FakeExercisesInfo()
 
-        response = self.client.put(
-            reverse("exercisesinfo-detail", args=[self.exercise1.id]),
-            data=data,
-            content_type=content_type,
+        response = self.client.patch(
+            reverse("exercises-info-detail", kwargs={"pk": self.exercise1.instance.id}),
+            data=new_exercise.request_create(),
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(
-            ExercisesInfo.objects.get(id=self.exercise1.id).title,
-            self.exercise2.title,
+            ExercisesInfo.objects.get(id=self.exercise1.instance.id).title,
+            new_exercise.request_create()["title"],
         )
 
     # 일반유저가 운동 정보를 수정할 수 있는지 확인
     def test_user_can_update_exercise(self):
-        self.client.login(**self.user_info)
-        response = self.client.put(
-            reverse("exercisesinfo-detail", args=[self.exercise1.id]),
-            data=self.exercise2_request,
+        new_exercise = FakeExercisesInfo()
+
+        self.client.force_login(self.user.instance)
+
+        response = self.client.patch(
+            reverse("exercises-info-detail", kwargs={"pk": self.exercise1.instance.id}),
+            data=new_exercise.request_create(),
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        self.assertEqual(
-            ExercisesInfo.objects.get(id=self.exercise1.id).title,
-            self.exercise1.title,
-        )
 
     # 관리자만 운동 정보를 삭제할 수 있는지 확인
     def test_admin_can_delete_exercise(self):
-        self.client.login(**self.admin_info)
+        self.client.force_login(self.admin.instance)
+
         response = self.client.delete(
-            reverse("exercisesinfo-detail", args=[self.exercise1.id])
+            reverse("exercises-info-detail", kwargs={"pk": self.exercise1.instance.id}),
         )
+
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(ExercisesInfo.objects.count(), 1)
 
     # 일반유저가 운동 정보를 삭제할 수 있는지 확인
     def test_user_can_delete_exercise(self):
-        self.client.login(**self.user_info)
+        self.client.force_login(self.user.instance)
         response = self.client.delete(
-            reverse("exercisesinfo-detail", args=[self.exercise1.id])
+            reverse("exercises-info-detail", kwargs={"pk": self.exercise1.instance.id}),
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(ExercisesInfo.objects.count(), 2)
 
     # 운동 정보 생성 시 필수 필드가 누락되었을 때 에러가 발생하는지 확인
     def test_exercise_create_error_when_required_field_is_missing(self):
-        self.client.login(**self.admin_info)
+        self.client.force_login(self.admin.instance)
         response = self.client.post(
-            reverse("exercisesinfo-list"),
+            reverse("exercises-info-list"),
             data={},
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
