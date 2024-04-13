4e0b3cdd58a2d09fc76d380b9fa9f03d2340ecc4
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-13 18:33:47 +0900

    🔧 (exercises_info): Refactor FocusAreaSerializer in serializers.py to include validation for focus_area field
    유효성 검사를 FocusSerializer 내부로 옮김

--- exercises_info/serializers.py
+++ exercises_info/serializers.py
@@ -11,6 +11,12 @@ class FocusAreaSerializer(serializers.ModelSerializer):
         model = FocusArea
         fields = ["focus_area"]
 
+    def validate_focus_area(self, value):
+        valid_focus_area_names = [enum.value for enum in FocusAreaEnum]
+        if value not in valid_focus_area_names:
+            raise ValidationError(f"{value} is not a valid focus area.")
+        return value
+
 
 class ExercisesInfoSerializer(WritableNestedModelSerializer):
     focus_areas = FocusAreaSerializer(many=True)
@@ -20,19 +26,3 @@ class ExercisesInfoSerializer(WritableNestedModelSerializer):
         model = ExercisesInfo
         fields = ["title", "author", "username", "description", "video", "focus_areas"]
         read_only_fields = ["author", "username"]
-
-    def validate_focus_areas(self, attr_data):
-        if not attr_data:
-            raise ValidationError("Focus areas are required.")
-        valid_focus_area_names = [enum.value for enum in FocusAreaEnum]
-        for focus_area_data in attr_data:
-            focus_area = focus_area_data.get("focus_area")
-            if focus_area not in valid_focus_area_names:
-                raise ValidationError(f"{focus_area} is not a valid focus area.")
-        return attr_data
-
-    def validate(self, data):
-        # Apply custom validations
-        if "focus_areas" in data:
-            self.validate_focus_areas(data["focus_areas"])
-        return data
