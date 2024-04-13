6e71f4449cc2e3aaa1db4a8a693074ae0dc63bf4
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-13 23:51:21 +0900

    ✅ (my_health_info): Add & Success test test_delete_routine_not_author

--- my_health_info/tests.py
+++ my_health_info/tests.py
@@ -452,3 +452,22 @@ class RoutineTestCase(TestCase):
         response = self.client.delete(reverse("routine-detail", kwargs={"pk": pk}))
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_routine_not_author(self):
+        """
+        본인이 생성한 루틴이 아닌 경우 루틴 삭제 요청이 403 에러를 리턴하는지 테스트
+
+        reverse_url: routine-detail
+        HTTP method: DELETE
+
+        테스트 시나리오:
+        1. 로그인한 유저가 /routine/<pk>/에 DELETE 요청을 보냅니다.
+        2. 403 에러를 리턴하는지 확인합니다.
+        """
+        self.client.force_login(self.user2.instance)
+
+        pk = self.routine1.instance.pk
+
+        response = self.client.delete(reverse("routine-detail", kwargs={"pk": pk}))
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
