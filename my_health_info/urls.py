9d2372b906038cef0c5cb22d9cbe0ecd070e09c3
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-13 23:14:55 +0900

    Add RoutineViewSet to my_health_info/urls.py

--- my_health_info/urls.py
+++ my_health_info/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from my_health_info.views import MyHealthInfoViewSet
+from my_health_info.views import MyHealthInfoViewSet, RoutineViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r"my-health-info", MyHealthInfoViewSet, basename="my-health-info")
+router.register(r"routine", RoutineViewSet, basename="routine")
 
 urlpatterns = [
     path("", include(router.urls)),
