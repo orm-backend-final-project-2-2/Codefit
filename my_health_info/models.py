a1594d9337c28c59161b29cc222ac9d2419334c1
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-13 23:14:42 +0900

    Add Routine model to my_health_info app

--- my_health_info/models.py
+++ my_health_info/models.py
@@ -17,3 +17,13 @@ class HealthInfo(models.Model):
     def save(self, *args, **kwargs):
         self.bmi = float(self.weight) / ((float(self.height) / 100) ** 2)
         super().save(*args, **kwargs)
+
+
+class Routine(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username}의 루틴 정보"
