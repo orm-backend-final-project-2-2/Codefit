6d6311861b8b887937828c4ba0b9a6db5bdb5e1e
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-14 17:01:34 +0900

    ✨  (my_health_info): Add subscribe_routine method

--- my_health_info/services.py
+++ my_health_info/services.py
@@ -1,9 +1,10 @@
 from account.models import CustomUser as User
 from my_health_info.models import Routine, UsersRoutine
-from rest_framework.exceptions import PermissionDenied
 
 
 class UsersRoutineManagementService:
     def __init__(self, user: User, routine: Routine):
         self.user = user
         self.routine = routine
+
+    def subscribe_routine(self): ...
