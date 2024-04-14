c09150edb43f97a1844e1941d81e008de9ab55d9
soohyun020812 <amd090108lsh02@gmail.com>
2024-04-14 14:21:28 +0900

    ✅ (community): 댓글 뷰셋 추가

--- community/urls.py
+++ community/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from community.views import PostViewSet
+from community.views import PostViewSet, CommentViewSet
 from rest_framework.routers import DefaultRouter
+from community import views
 
 router = DefaultRouter()
 router.register(r"posts", PostViewSet, basename="post")
+router.register(r"comments", CommentViewSet, basename="comment")
+
 
 urlpatterns = [
     path("", include(router.urls)),
