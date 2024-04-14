42f28d7f26dee58288cfe3d62b2a1c052c9036b8
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-15 04:04:32 +0900

    🔧 (my_health_info): Update RoutineViewSet destroy method to delete subscribers and routine

--- my_health_info/views.py
+++ my_health_info/views.py
@@ -224,6 +224,18 @@ class RoutineViewSet(viewsets.ModelViewSet):
 
         return Response(data, status=status.HTTP_200_OK)
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        authors_users_routine = instance.subscribers.filter(
+            user=instance.author
+        ).first()
+        authors_users_routine.delete()
+
+        instance.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
     @action(
         detail=True,
         methods=["post"],
