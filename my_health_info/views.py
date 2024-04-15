007c5205c62dccad7aa9f3dea739745d1692c32d
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-15 16:37:40 +0900

    📝(my_health_info): Update Docstring to views.py

--- my_health_info/views.py
+++ my_health_info/views.py
@@ -55,13 +55,29 @@ class MyHealthInfoViewSet(viewsets.ModelViewSet):
         return HealthInfo.objects.filter(user=self.request.user).order_by("-created_at")
 
     def list(self, request, *args, **kwargs):
-        """최근 35일간의 건강 정보를 조회"""
-        queryset = self.get_queryset().all()[: self.days_to_show]
+        """
+        건강 정보를 리스트로 반환
+
+        - 최근 35일간의 건강 정보만 조회 가능
+        """
+        queryset = (
+            self.get_queryset()
+            .all()
+            .filter(
+                created_at__gte=timezone.now()
+                - timezone.timedelta(days=self.days_to_show)
+            )
+        )
+
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
     def perform_create(self, serializer):
-        """건강 정보 생성 시 user 정보를 추가"""
+        """
+        새로운 건강 정보 생성
+
+        이미 오늘의 건강 정보가 존재한다면 400 에러 반환
+        """
 
         last_health_info = self.get_queryset()
         if last_health_info.exists():
@@ -73,7 +89,11 @@ class MyHealthInfoViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=["get"], url_path="last", url_name="last")
     def last(self, request, *args, **kwargs):
-        """가장 최근 생성된 건강 정보 조회"""
+        """
+        가장 최근의 건강 정보 조회
+
+        사용자의 건강 정보가 없다면 404 에러 반환
+        """
         queryset = self.get_queryset()
         if queryset.exists():
             serializer = self.get_serializer(queryset.first())
@@ -91,6 +111,7 @@ class RoutineViewSet(viewsets.ModelViewSet):
     - list: GET /my_health_info/routine/
     - create: POST /my_health_info/routine/
     - retrieve: GET /my_health_info/routine/<pk>/
+    - partial_update: PATCH /my_health_info/routine/<pk>/
     - subscribe: POST /my_health_info/routine/<pk>/subscribe/
     """
 
@@ -100,9 +121,21 @@ class RoutineViewSet(viewsets.ModelViewSet):
     ordering_fields = ["like_count"]
 
     def get_queryset(self):
+        """
+        루틴 정보를 반환, 삭제되지 않은 루틴만 조회
+        """
         return Routine.objects.filter(is_deleted=False)
 
     def order_queryset(self, queryset):
+        """
+        주어진 쿼리셋을 ordering에 따라 정렬
+
+        기본 ordering은 -created_at
+        ordering이 없다면 기본 ordering을 사용
+
+        추가 ordering은 query_params의 ordering 리스트 + 기본 ordering 순으로 배열
+        그 후 order_by 함수를 사용하여 정렬
+        """
         base_ordering = ["-created_at"]
 
         orderings = self.request.query_params.get("ordering", None)
@@ -123,6 +156,15 @@ class RoutineViewSet(viewsets.ModelViewSet):
         return queryset
 
     def search_queryset(self, queryset):
+        """
+        주어진 쿼리셋을 검색어에 따라 필터링
+
+        query_params의 search에 해당하는 필드를 검색어로 포함하는 루틴만 반환
+
+        검색어가 없다면 주어진 쿼리셋 그대로 반환
+
+        Q 객체를 사용하여 검색어에 해당하는 필드를 필터링
+        """
         author__id = self.request.query_params.get("author__id", None)
 
         if not author__id:
@@ -133,6 +175,13 @@ class RoutineViewSet(viewsets.ModelViewSet):
         return queryset
 
     def list(self, request, *args, **kwargs):
+        """
+        루틴 정보를 리스트로 반환
+
+        주어진 쿼리셋을 검색어와 ordering에 따라 필터링 및 정렬
+
+        그 후 serializer를 사용하여 데이터 반환
+        """
         queryset = self.get_queryset()
         queryset = self.search_queryset(queryset)
         queryset = self.order_queryset(queryset)
@@ -142,12 +191,26 @@ class RoutineViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     def retrieve(self, request, *args, **kwargs):
+        """
+        특정 루틴 정보 조회
+
+        주어진 pk에 해당하는 루틴 정보를 반환
+        """
         instance = self.get_object()
         serializer = self.get_serializer(instance)
 
         return Response(serializer.data)
 
     def perform_create(self, serializer):
+        """
+        루틴을 생성하는 로직
+
+        1. validated_data에서 외부 테이블의 데이터를 pop
+        2. 루틴을 생성
+        3. 루틴을 복제하여 MirroredRoutine 생성
+        4. 루틴에 연결된 운동들을 ExerciseInRoutine으로 생성한 후 Routine과 MirroredRoutine에 연결
+        5. UsersRoutineManagementService를 통해 UsersRoutine 생성
+        """
         exercises_in_routine_data = serializer.validated_data.pop(
             "exercises_in_routine", []
         )
@@ -174,6 +237,23 @@ class RoutineViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def partial_update(self, request, *args, **kwargs):
+        """
+        루틴의 일부를 수정하는 로직
+
+        1. 루틴 인스턴스를 가져옴
+        2. 뷰셋과 연결된 serializer를 가져옴
+        3. serializer.is_valid()를 통해 데이터 유효성 검사
+        4. request.data에 exercises_in_routine이 있다면
+        5. ExerciseInRoutine의 Routine을 None으로 수정하여 연결 해제
+        6. Routine에 연결된 MirroredRoutine의 original_routine을 None으로 수정하여 연결 해제
+        7. 새로운 MirroredRoutine 생성
+        8. 새로운 ExerciseInRoutine를 생성하여 Routine과 MirroredRoutine에 연결
+        9. Routine의 subscribers를 가져와서 순회
+        10. subscriber == author의 경우 mirrored_routine을 새로운 MirroredRoutine으로 수정
+        11. subscriber != author의 경우 mirrored_routine을 업데이트하는 대신 need_update를 True로 수정
+        12. 나머지 Routine의 정보를 업데이트
+        13. 업데이트된 정보를 반환
+        """
         kwargs["partial"] = True
 
         instance = self.get_object()
@@ -228,14 +308,26 @@ class RoutineViewSet(viewsets.ModelViewSet):
         return Response(data, status=status.HTTP_200_OK)
 
     def destroy(self, request, *args, **kwargs):
+        """
+        루틴을 삭제하는 로직
+
+        1. 루틴 인스턴스를 가져옴
+        2. 루틴의 author의 UsersRoutine을 가져옴
+        3. author의 UsersRoutine의 routine을 None으로 수정
+        4. 루틴의 is_deleted를 True로 수정
+        5. 204 응답 반환
+        """
         instance = self.get_object()
 
-        authors_users_routine = instance.subscribers.filter(
-            user=instance.author
-        ).first()
-        authors_users_routine.delete()
+        try:
+            authors_users_routine = instance.subscribers.get(user=instance.author)
+            authors_users_routine.routine = None
+            authors_users_routine.save()
+        except:
+            raise NotFound("Author's routine not found")
 
-        instance.delete()
+        instance.is_deleted = True
+        instance.save()
 
         return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -269,6 +361,13 @@ class RoutineViewSet(viewsets.ModelViewSet):
         permission_classes=[IsAuthenticated],
     )
     def subscribe(self, request, *args, **kwargs):
+        """
+        유저가 루틴을 구독하는 로직
+
+        1. 루틴과 유저 인스턴스를 가져옴
+        2. UsersRoutineManagementService를 통해 유저가 루틴을 구독하게 함
+        3. 구독된 UsersRoutine을 반환
+        """
         routine = self.get_object()
         user = request.user
         service = UsersRoutineManagementService(user=user, routine=routine)
@@ -297,9 +396,15 @@ class UsersRoutineViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        """
+        본인이 소유한 루틴 정보를 반환
+        """
         return UsersRoutine.objects.filter(user=self.request.user)
 
     def list(self, request, *args, **kwargs):
+        """
+        유저가 소유한 UsersRoutine 정보를 리스트로 반환
+        """
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
 
@@ -321,7 +426,12 @@ class WeeklyRoutineView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        """유저의 주간 루틴 정보 조회"""
+        """
+        유저의 주간 루틴 정보 조회
+
+        1. 쿼리셋에서 유저의 주간 루틴 정보를 조회하여 요일 순으로 정렬
+        2. WeeklyRoutineSerializer를 사용하여 데이터 반환
+        """
         user = request.user
         queryset = WeeklyRoutine.objects.filter(user=user).order_by("day_index")
         serializer = WeeklyRoutineSerializer(queryset, many=True)
